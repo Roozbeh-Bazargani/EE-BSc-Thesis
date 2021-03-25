1330dc35c94c5565e04f0a2df591fcd6153bb1ec
roozbeh bazargani <roozbehbazargani@gmail.com>
2021-03-25 20:11:57 +0430

    added and commented the considering obstacles around actual obstacles

--- MATLAB test/map_R.m
+++ MATLAB test/map_R.m
@@ -9,6 +9,7 @@ p(size(p,1),:) = ones(1, size(p,2));
 p(:,1) = ones(size(p,1),1);
 p(:,size(p,2)) = ones(size(p,1),1);
 %p(:,13) = [1; 0; 0; 0; 0; 0; 0; ones(20,1)];
+%raw_objects = [20 25; 20 24; 20 23; 20 22; 20 21; 20 20; 20 19];
 objects = [20 25; 20 24; 20 23; 20 22; 20 21; 20 20; 20 19];
 %objects = [1 2; 1 39];
 loc = zeros(size(p,2), size(p,1));
@@ -16,6 +17,21 @@ loc(1,:) = ones(1, size(loc,2));
 loc(size(loc,1),:) = ones(1, size(loc,2));
 loc(:,1) = ones(size(loc,1),1);
 loc(:,size(loc,2)) = ones(size(loc,1),1);
+
+%objects = zeros(size(p,1)*size(p,2), 2);
+% neighbours = {N, S, E, W, NW, NE, SW, SE, 0} |neigbours| = 9
+%neighbours = [0 1; 0 -1; 1 0; -1 0; -1 1; 1 1; -1 -1; 1 -1; 0 0]; % direction of neigbours in x-y coordination
+%numNeighbours = 9;
+%k = 1;
+% for i = 1:1:size(raw_objects,1)
+%     p(size(p,1) - raw_objects(i,2) + 1, raw_objects(i,1) + 1) = 1;
+%     for j = 1:1:numNeighbours
+%         objects(k,:) = raw_objects(i,:) + neighbours(j,:);
+%         k = k + 1;
+%     end
+% end
+% [x, ~] = find(objects==0);
+% objects = objects(1:x(1) - 1,:);
 
 for i = 1:1:size(objects,1)
     p(size(p,1) - objects(i,2) + 1, objects(i,1) + 1) = 1;
